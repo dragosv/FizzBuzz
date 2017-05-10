20a6f66cdaea182eb26467d0527137b8ea3e7501
Dragos Varovici <dvarovici.work@gmail.com>
2017-05-10 12:57:51 -0600

    Numbers specs

--- app/src/test/NumbersSpec.kt
+++ app/src/test/NumbersSpec.kt
@@ -6,11 +6,21 @@ import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
 import kotlin.test.assertEquals
 
-class SampleSpec : Spek({
-    given("some context") {
-        on("executing some action") {
-            it("should pass") {
-                assertEquals(2, 2)
+class NumbersSpec : Spek({
+    given("numbers") {
+        on("retrieving first 10") {
+            val numbers = Numbers.getNumbers(10)
+
+            it("should have expected primes") {
+                val primes = numbers.filter { it.isPrime }.map { it.value }.toList()
+
+                assertEquals(listOf(1,2, 3, 5, 7), primes)
+            }
+
+            it("should have expected fibonaci") {
+                val primes = numbers.filter { it.isFibonaci }.map { it.value }.toList()
+
+                assertEquals(listOf(1,2, 3, 5, 8), primes)
             }
         }
     }
