26ff4462a220382713dd16ce0a9e39d0cfb25cb2
Dragos Varovici <dvarovici.work@gmail.com>
2017-05-10 13:06:48 -0600

    Using expekt for assertions

--- app/src/test/NumbersSpec.kt
+++ app/src/test/NumbersSpec.kt
@@ -1,5 +1,6 @@
 package org.dragosv.fizzbuzz
 
+import com.winterbe.expekt.should
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -14,13 +15,13 @@ class NumbersSpec : Spek({
             it("should have expected primes") {
                 val primes = numbers.filter { it.isPrime }.map { it.value }.toList()
 
-                assertEquals(listOf(1,2, 3, 5, 7), primes)
+                listOf(1,2, 3, 5, 7).should.equal(primes)
             }
 
             it("should have expected fibonaci") {
-                val primes = numbers.filter { it.isFibonaci }.map { it.value }.toList()
+                val fibonacis = numbers.filter { it.isFibonaci }.map { it.value }.toList()
 
-                assertEquals(listOf(1,2, 3, 5, 8), primes)
+                listOf(1,2, 3, 5, 8).should.equal(fibonacis)
             }
         }
     }
